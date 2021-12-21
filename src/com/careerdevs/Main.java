7b7e2efd24f64b36635c91b6d812051ebc158649
TheseTimesAreHard <erickromah.student@careerdev.com>
2021-12-20 20:55:08 -0500

    Initial commit

---
+++ src/com/careerdevs/Main.java
@@ -0,0 +1,14 @@
+package com.careerdevs;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+
+    }
+    public static int getFirstValue(int[] arr)
+    {
+        return arr[0];
+    }
+}
