6dc9f2b07b81f280f592a82767640c9781e27cbf
TheseTimesAreHard <erickromah.student@careerdev.com>
2021-12-20 21:12:18 -0500

    Implement addation algorithum

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -5,10 +5,17 @@ public class Main
 
     public static void main(String[] args)
     {
-
+        System.out.println(getFirstValue(new int[]{1, 2, 3, 4}));
+        System.out.println(addition(7));
     }
     public static int getFirstValue(int[] arr)
     {
         return arr[0];
     }
+
+    public static int addition(int num)
+    {
+        return num + 1;
+    }
+
 }
