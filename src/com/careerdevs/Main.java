9c22618aff9219d03778d1d9822711d16759fbec
TheseTimesAreHard <erickromah.student@careerdev.com>
2021-12-23 18:37:20 -0500

    Implement addation algorithum

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -7,6 +7,9 @@ public class Main
     {
         System.out.println(getFirstValue(new int[]{1, 2, 3, 4}));
         System.out.println(addition(7));
+        System.out.println(convert(5));
+        System.out.println(giveMeSomething("got me fucked up lul"));
+        System.out.println(search());
     }
     public static int getFirstValue(int[] arr)
     {
@@ -17,5 +20,18 @@ public class Main
     {
         return num + 1;
     }
+
+    public static int convert(int minutes) { return minutes * 60;}
+
+    public static String giveMeSomething(String a) {return "something " + a;}
+
+    public static int search(int arr[], int item) {	int index=-1;
+        for(int i=0; i < arr.length;i++){
+            if(arr[i]==item){
+                index = i;
+                i+=arr.length+1;
+            }
+        }
+        return index;}
 
 }
